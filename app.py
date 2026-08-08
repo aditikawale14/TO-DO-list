4c2f115b4accd49277c5d97aec4cf152f490f9e6
aditikawale14 <aditikawale146@gmail.com>
2026-08-08 14:59:51 +0530

    updated index.html tables

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 todos = [
     {"sno":1,"tilte":"Sample task",
-     "desc":"This is a sample task for todo list","status":"Pending"}
+     "desc":"This is a sample task for todo list", "date_created": "08-08-26", "status":"Pending"}
 ]
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",allTodos = todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
